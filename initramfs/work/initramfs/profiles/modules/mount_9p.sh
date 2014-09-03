200bdcd67b10da5776730fb2ffbc44f34cb9e5a9
yukipyon <wolfvbear@gmail.com>
2014-09-03 20:37:37 +0100

    Added default arguments.

--- initramfs/work/initramfs/profiles/modules/mount_9p.sh
+++ initramfs/work/initramfs/profiles/modules/mount_9p.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+if [ -z $1 ] && [ -z $2 ]
+then
+	src=share
+	dst=/share
+else
+	src=$1
+	dst=$2
+fi
+	
 #TODO Might want multiple shares with different names at some point.
 #i.e. one for logs...
-mount -t 9p -o trans=virtio,noexec share /share
+mount -t 9p -o trans=virtio,noexec "$src" "$dst"
